dc6fa510797c10556938e875b4ee23ce45e8cb30
josaltech <josalgallego@gmail.com>
2019-10-14 20:44:53 -0500

    Initialize Repository

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { UserComponent } from './user/user.component';
+
+@NgModule({
+  declarations: [AppComponent, HolaMundoComponent, UserComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
